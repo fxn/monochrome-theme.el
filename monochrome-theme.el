d5362fe7ae03c2c86d24dddfc5493adbc2d4f2e1
Xavier Noria <fxn@hashref.com>
2011-11-24 22:04:51 +0100

    refinement: more grays, a little bit darker

--- monochrome-theme.el
+++ monochrome-theme.el
@@ -1,7 +1,7 @@
 ;;; monochrome-theme.el --- monochrome color theme
 
 ;; Copyright (C) 2011 Xavier Noria
-
+;;
 ;; Author: Xavier Noria <fxn@hashref.com>
 ;;
 ;; This theme is based on the builtin dichromacy theme, it is basically
@@ -9,7 +9,7 @@
 ;;
 ;; Just throw it into ~/.emacs.d and
 ;;
-;;     M-x load-theme RET monochrome
+;;     M-x load-theme RET monochrome RET
 ;;
 ;; or put in your init file
 ;;
@@ -20,21 +20,27 @@
 (deftheme monochrome
   "Monochrome color theme based on dichromacy.")
 
-(let ((class '((class color) (min-colors 10))))
+(let ((class '((class color) (min-colors 10)))
+      (black "black")
+      (white "white")
+      (lgray "light gray")
+      (dgray "dark gray")
+      (sgray "light slate gray"))
+
   (custom-theme-set-faces
    'monochrome
 
-   `(default ((,class (:foreground "white" :background "black"))))
-   `(cursor ((,class (:background "white"))))
+   `(default ((,class (:foreground ,lgray :background ,black))))
+   `(cursor ((,class (:background ,lgray))))
 
    ;; Highlighting faces
-   `(fringe ((,class (:background "white"))))
-   `(highlight ((,class (:foreground "black" :background "white"))))
-   `(region ((,class (:foreground "black" :background "white"))))
-   `(secondary-selection ((,class (:foreground: "black" :background "#f7f7f7"))))
-   `(isearch ((,class (:foreground "black" :background "white"))))
-   `(lazy-highlight ((,class (:foreground "black" :background "white"))))
-   `(trailing-whitespace ((,class (:background "white"))))
+   `(fringe ((,class (:background ,lgray))))
+   `(highlight ((,class (:foreground ,black :background ,lgray))))
+   `(region ((,class (:foreground ,black :background ,lgray))))
+   `(secondary-selection ((,class (:foreground: ,black :background ,sgray))))
+   `(isearch ((,class (:foreground "black" :background ,lgray))))
+   `(lazy-highlight ((,class (:foreground "black" :background ,lgray))))
+   `(trailing-whitespace ((,class (:background ,lgray))))
 
    ;; Mode line faces
    `(mode-line ((,class (:box (:line-width -1 :style released-button)
@@ -44,68 +50,69 @@
 				       :foreground "black"))))
 
    ;; Escape and prompt faces
-   `(minibuffer-prompt ((,class (:weight bold :foreground "white"))))
-   `(escape-glyph ((,class (:foreground "white"))))
+   `(minibuffer-prompt ((,class (:weight bold :foreground ,lgray))))
+   `(escape-glyph ((,class (:foreground ,lgray))))
    `(error ((,class (:weight bold :slant italic
 			     :foreground "red"))))
    `(warning ((,class (:foreground "yellow"))))
    `(success ((,class (:foreground "gree"))))
 
    ;; Font lock faces
-   `(font-lock-builtin-face ((,class (:foreground "white"))))
-   `(font-lock-comment-face ((,class (:slant italic :foreground "white"))))
-   `(font-lock-constant-face ((,class (:weight bold :foreground "white"))))
-   `(font-lock-function-name-face ((,class (:foreground "white"))))
-   `(font-lock-keyword-face ((,class (:weight bold :foreground "white"))))
-   `(font-lock-string-face ((,class (:foreground "white"))))
-   `(font-lock-type-face ((,class (:weight bold :foreground "white"))))
-   `(font-lock-variable-name-face ((,class (:weight bold :foreground "white"))))
+   `(font-lock-builtin-face ((,class (:foreground ,lgray))))
+   `(font-lock-comment-face ((,class (:slant italic :foreground ,dgray))))
+   `(font-lock-constant-face ((,class (:weight bold :foreground ,lgray))))
+   `(font-lock-function-name-face ((,class (:foreground ,white))))
+   `(font-lock-keyword-face ((,class (:weight bold :foreground ,white))))
+   `(font-lock-string-face ((,class (:foreground ,sgray))))
+   `(font-lock-type-face ((,class (:weight bold :foreground ,lgray))))
+   `(font-lock-variable-name-face ((,class (:weight bold :foreground ,sgray))))
 
    ;; Button and link faces
-   `(link ((,class (:underline t :foreground "white"))))
-   `(link-visited ((,class (:underline t :foreground "white"))))
+   `(link ((,class (:underline t :foreground ,lgray))))
+   `(link-visited ((,class (:underline t :foreground ,lgray))))
 
    ;; Gnus faces
-   `(gnus-group-news-1 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-news-1-low ((,class (:foreground "white"))))
-   `(gnus-group-news-2 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-news-2-low ((,class (:foreground "white"))))
-   `(gnus-group-news-3 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-news-3-low ((,class (:foreground "white"))))
-   `(gnus-group-news-4 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-news-4-low ((,class (:foreground "white"))))
-   `(gnus-group-news-5 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-news-5-low ((,class (:foreground "white"))))
-   `(gnus-group-news-low ((,class (:foreground "white"))))
-   `(gnus-group-mail-1 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-mail-1-low ((,class (:foreground "white"))))
-   `(gnus-group-mail-2 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-mail-2-low ((,class (:foreground "white"))))
-   `(gnus-group-mail-3 ((,class (:weight bold :foreground "white"))))
-   `(gnus-group-mail-3-low ((,class (:foreground "white"))))
-   `(gnus-group-mail-low ((,class (:foreground "white"))))
-   `(gnus-header-content ((,class (:foreground "white"))))
-   `(gnus-header-from ((,class (:weight bold :foreground "white"))))
-   `(gnus-header-subject ((,class (:foreground "white"))))
-   `(gnus-header-name ((,class (:foreground "white"))))
-   `(gnus-header-newsgroups ((,class (:foreground "white"))))
+   `(gnus-group-news-1 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-news-1-low ((,class (:foreground ,lgray))))
+   `(gnus-group-news-2 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-news-2-low ((,class (:foreground ,lgray))))
+   `(gnus-group-news-3 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-news-3-low ((,class (:foreground ,lgray))))
+   `(gnus-group-news-4 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-news-4-low ((,class (:foreground ,lgray))))
+   `(gnus-group-news-5 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-news-5-low ((,class (:foreground ,lgray))))
+   `(gnus-group-news-low ((,class (:foreground ,lgray))))
+   `(gnus-group-mail-1 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-mail-1-low ((,class (:foreground ,lgray))))
+   `(gnus-group-mail-2 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-mail-2-low ((,class (:foreground ,lgray))))
+   `(gnus-group-mail-3 ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-group-mail-3-low ((,class (:foreground ,lgray))))
+   `(gnus-group-mail-low ((,class (:foreground ,lgray))))
+   `(gnus-header-content ((,class (:foreground ,lgray))))
+   `(gnus-header-from ((,class (:weight bold :foreground ,lgray))))
+   `(gnus-header-subject ((,class (:foreground ,lgray))))
+   `(gnus-header-name ((,class (:foreground ,lgray))))
+   `(gnus-header-newsgroups ((,class (:foreground ,lgray))))
+
    ;; Message faces
-   `(message-header-name ((,class (:foreground "white"))))
-   `(message-header-cc ((,class (:foreground "white"))))
-   `(message-header-other ((,class (:foreground "white"))))
-   `(message-header-subject ((,class (:foreground "white"))))
-   `(message-header-to ((,class (:weight bold :foreground "white"))))
-   `(message-cited-text ((,class (:slant italic :foreground "white"))))
-   `(message-separator ((,class (:weight bold :foreground "white"))))
+   `(message-header-name ((,class (:foreground ,lgray))))
+   `(message-header-cc ((,class (:foreground ,lgray))))
+   `(message-header-other ((,class (:foreground ,lgray))))
+   `(message-header-subject ((,class (:foreground ,lgray))))
+   `(message-header-to ((,class (:weight bold :foreground ,lgray))))
+   `(message-cited-text ((,class (:slant italic :foreground ,lgray))))
+   `(message-separator ((,class (:weight bold :foreground ,lgray))))
    ;; Flyspell
    `(flyspell-duplicate ((,class (:weight unspecified :foreground unspecified
-				  :slant unspecified :underline "white"))))
+				  :slant unspecified :underline ,lgray))))
    `(flyspell-incorrect ((,class (:weight unspecified :foreground unspecified
-				  :slant unspecified :underline "white")))))
+				  :slant unspecified :underline ,lgray)))))
 
   (custom-theme-set-variables
    'monochrome
-   `(ansi-color-names-vector ["black" "white"])))
+   `(ansi-color-names-vector ["black" ,lgray])))
 
 (provide-theme 'monochrome)
 
